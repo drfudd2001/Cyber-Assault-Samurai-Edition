9b9a298af293df15745b80f8c67b6f293746c57d
drfudd2001 <elmer.figueroagarcia@gmail.com>
2022-02-23 19:09:58 -0600

    Update game.js

--- game.js
+++ game.js
@@ -8,14 +8,11 @@ let inputDir = { x: 0, y: 0 };
 const foodSound = new Audio('assets/food.wav');
 const gameOverSound = new Audio('assets/game-over.wav');
 const moveSound = new Audio('assets/move.wav');
-//const musicSound = new Audio('assets/game-music.mp3');
 let speed = 10;
 let score = 0;
 let level = 0
 let lastDrawTime = 0;
-let snakeArr = [
-    { x: 13, y: 15 }
-];
+let snakeArr = [{ x: 13, y: 15 }];
 
 food = { x: 6, y: 7 };
 
@@ -48,11 +45,9 @@ function gameEngine() {
     // Part 1: Updating the snake array & Food
     if (isCollide(snakeArr)) {
         gameOverSound.play();
-        //musicSound.pause();
         inputDir = { x: 0, y: 0 };
         alert("Game Over Samurai. Press any key to get those Corpos again!");
         snakeArr = [{ x: 13, y: 15 }];
-        //musicSound.play();
         score = 0;
     }
     // If you have eaten the food, increment the score and regenerate the food
@@ -102,7 +97,6 @@ function gameEngine() {
 }
 
 // Main logic starts here
-//musicSound.play();
 let highScore = localStorage.getItem("high score");
 if (highScore === null) {
     highScoreVal = 0;
@@ -118,22 +112,18 @@ window.addEventListener('keydown', e => {
     moveSound.play();
     switch (e.key) {
         case "ArrowUp":
-            console.log("ArrowUp");
             inputDir.x = 0;
             inputDir.y = -1;
             break;
         case "ArrowDown":
-            console.log("ArrowDown");
             inputDir.x = 0;
             inputDir.y = 1;
             break;
         case "ArrowLeft":
-            console.log("ArrowLeft");
             inputDir.x = -1;
             inputDir.y = 0;
             break;
         case "ArrowRight":
-            console.log("ArrowRight");
             inputDir.x = 1;
             inputDir.y = 0;
             break;
