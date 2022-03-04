3dfb8f0a43fb5f87a7fdbc711dcb637d4f8a43b0
drfudd2001 <elmer.figueroagarcia@gmail.com>
2022-03-04 16:52:46 -0600

    Working game

--- game.js
+++ game.js
@@ -5,14 +5,14 @@ for playing. I will attempt to create a save-state that allows player to continu
 
 // Game Constants & Variables
 let inputDir = { x: 0, y: 0 };
+let lastInputDir = { x: 0, y: 0};
 const foodSound = new Audio('assets/food.wav');
 const gameOverSound = new Audio('assets/game-over.wav');
 const moveSound = new Audio('assets/move.wav');
 let speed = 10;
 let score = 0;
-let level = 0
 let lastDrawTime = 0;
-let snakeArr = [{ x: 13, y: 15 }];
+let snakeBody = [{ x: 11, y: 11 }];
 food = { x: 6, y: 7 };
 
 // Game Functions
@@ -30,7 +30,7 @@ function main(time) {
 
 function isCollide(snake) {
     // If you bump into yourself 
-    for (let i = 1; i < snakeArr.length; i++) {
+    for (let i = 1; i < snakeBody.length; i++) {
         if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
             return true;
         }
@@ -44,15 +44,15 @@ function isCollide(snake) {
 
 function gameEngine() {
     // Part 1: Updating the snake array & Food
-    if (isCollide(snakeArr)) {
+    if (isCollide(snakeBody)) {
         gameOverSound.play();
         inputDir = { x: 0, y: 0 };
         alert("Game Over Samurai. Press any key to get those Corpos again!");
-        snakeArr = [{ x: 13, y: 15 }];
+        snakeBody = [{ x: 13, y: 15 }];
         score = 0;
     }
     // If you have eaten the food, increment the score and regenerate the food
-    if (snakeArr[0].y === food.y && snakeArr[0].x === food.x) {
+    if (snakeBody[0].y === food.y && snakeBody[0].x === food.x) {
         foodSound.play();
         score += 1;
         if (score > highScoreVal) {
@@ -61,32 +61,28 @@ function gameEngine() {
             highScoreBox.innerHTML = "High Score: " + highScoreVal;
         }
         scoreBox.innerHTML = "Score: " + score;
-        snakeArr.unshift({ x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y });
+        snakeBody.unshift({ x: snakeBody[0].x + inputDir.x, y: snakeBody[0].y + inputDir.y });
         let a = 2;
         let b = 16;
         food = {x: Math.round(a + (b - a)*Math.random()), y: Math.round(a + (b - a)*Math.random())}
     }
 
     // Moving the snake
-    for (let i = snakeArr.length - 2; i >= 0; i--) {
-        snakeArr[i + 1] = {...snakeArr[i] };
+    for (let i = snakeBody.length - 2; i >= 0; i--) {
+        snakeBody[i + 1] = {...snakeBody[i] };
     }
 
-    snakeArr[0].x += inputDir.x;
-    snakeArr[0].y += inputDir.y;
+    snakeBody[0].x += inputDir.x;
+    snakeBody[0].y += inputDir.y;
 
     // Part 2: Display the snake and Food
     // Display the snake
     board.innerHTML = "";
-    snakeArr.forEach((e, index) => {
+    snakeBody.forEach(e => {
         snakeElement = document.createElement('div');
         snakeElement.style.gridRowStart = e.y;
         snakeElement.style.gridColumnStart = e.x;
-        if (index === 0) {
-            snakeElement.classList.add('head');
-        } else {
-            snakeElement.classList.add('snake');
-        }
+        snakeElement.classList.add('snake');
         board.appendChild(snakeElement);
     });
     // Display the food
@@ -108,28 +104,30 @@ if (highScore === null) {
     highScoreBox.innerHTML = "High Score: " + highScore;
 }
 
-window.requestAnimationFrame(main);
-window.addEventListener('keydown', e => {
-    inputDir = { x: 0, y: 1 }
-    moveSound.play();
+window.addEventListener("keydown", e =>{
     switch (e.key) {
         case "ArrowUp":
-            inputDir.x = 0;
-            inputDir.y = -1;
-            break;
+            if (lastInputDir.y !== 0) break
+            inputDir = {x: 0, y: -1}
+            break
         case "ArrowDown":
-            inputDir.x = 0;
-            inputDir.y = 1;
-            break;
+            if (lastInputDir.y !== 0) break
+            inputDir = {x: 0, y: 1}
+            break
         case "ArrowLeft":
-            inputDir.x = -1;
-            inputDir.y = 0;
-            break;
+            if (lastInputDir.x !== 0) break
+            inputDir = {x: -1, y: 0}
+            break
         case "ArrowRight":
-            inputDir.x = 1;
-            inputDir.y = 0;
-            break;
-        default:
-            break;
+            if (lastInputDir.x !== 0) break
+            inputDir = {x: 1, y: 0}
+            break
     }
 })
+
+function getInputDir(){
+    lastInputDir = inputDir
+    return inputDir
+}
+
+window.requestAnimationFrame(main);
